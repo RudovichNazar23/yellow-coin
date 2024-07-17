94f994f1c3e739d347322605d8d832c1aac6aaf2
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-07-17 21:33:03 +0200

    added jwt auth to the backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.isAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 

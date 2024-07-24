9950d1b43a8db40cbda2a695b77f61dbe97c3203
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-07-24 16:41:40 +0200

    changed the order of form and logo at the form container component

--- frontend/src/components/Pages/FormContainer.jsx
+++ frontend/src/components/Pages/FormContainer.jsx
@@ -19,15 +19,15 @@ export default function FormContainer({ initial }){
     return (
         <div class="container-fluid d-flex flex-column align-items-center justify-content-center">
             <div className="row w-100 h-100 d-flex flex-column flex-md-row align-items-center justify-content-between">
-                <IconDiv/>
-                <div class="col-12 col-md-1 d-none d-md-block d-flex flex-column align-items-center justify-content-center" style={{height: "900px"}}>
-                    <div class="vr" style={{height: "100%"}}></div>
-                </div>
                 <div className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center mt-4">
                     {
                         isOpened ? <LoginForm clickHandler={onClickHandler}/> : <RegistrationForm clickHandler={onClickHandler} />
                     }
                 </div>
+                <div class="col-12 col-md-1 d-none d-md-block d-flex flex-column align-items-center justify-content-center" style={{height: "900px"}}>
+                    <div class="vr" style={{height: "100%"}}></div>
+                </div>
+                <IconDiv/>
             </div>
         </div>
     );

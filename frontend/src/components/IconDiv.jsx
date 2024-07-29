4c248c652909363b1edf5c2e8dedff9f7efc3c23
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-07-29 18:25:42 +0200

    added responsive elements ordering at login/registration pages for any type of device

--- frontend/src/components/IconDiv.jsx
+++ frontend/src/components/IconDiv.jsx
@@ -1,6 +1,6 @@
 export default function IconDiv(){
     return (
-        <div className="col-12 col-md-5 d-flex flex-column align-items-center justify-content-center">
+        <div className="col-12 col-md-5 d-flex flex-column align-items-center justify-content-center order-lg-3 order-md-3">
             <div>
                 <img src="/coin.png" />
             </div>

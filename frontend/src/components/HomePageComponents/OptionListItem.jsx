0475becb8a3e2e0f7953c9c4b17db78aaa1846e7
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-07-30 12:31:41 +0200

    added new icons to the home page component

--- frontend/src/components/HomePageComponents/OptionListItem.jsx
+++ frontend/src/components/HomePageComponents/OptionListItem.jsx
@@ -1,6 +1,6 @@
 export default function OptionListItem({ textValue, iconName }){
     return(
-        <a className="col-lg-7 col-md-12 text-decoration-none text-dark" href="#">
+        <a className="col-lg-7 col-md-12 text-decoration-none text-dark d-flex" href="#">
             <img src={`/${iconName}`} width={"30"} height={"30"}/>
             <span className="d-none d-sm-inline ms-1">{textValue}</span>
         </a>

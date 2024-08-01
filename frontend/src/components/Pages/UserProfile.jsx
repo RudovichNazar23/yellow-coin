a7dab44772680a46c1bcd9b301a9a15c8ec4ca37
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-08-01 11:00:50 +0200

    added handler for deleting user profile

--- frontend/src/components/Pages/UserProfile.jsx
+++ frontend/src/components/Pages/UserProfile.jsx
@@ -1,14 +1,30 @@
+import api from "../../utils/api";
 import { useContext, useState } from "react";
 import { HomePageContext } from "../../context/HomePageContext";
 
 import ProfilePicture from "../UserProfileComponents/ProfilePicture";
 import Banner from "../UserProfileComponents/Banner";
 import InfoGroup from "../UserProfileComponents/InfoGorup";
-import ProfileButton from "../UserProfileComponents/ProfileButton";
+import DeleteButton from "../UserProfileComponents/DeleteButton";
+import EditButton from "../UserProfileComponents/EditButton";
+import ModalContainer from "../UserProfileComponents/ModalContainer";
 
 
 export default function UserProfile(){
     const { user } = useContext(HomePageContext);
+
+    const onDeleteHandler = (event) => {
+        event.preventDefault();
+        const response = api.delete(`/user_profile/user/${user.id}/`);
+        response
+        .then((res) => {
+            if(res.status === 204){
+                localStorage.clear();
+                window.location.reload();
+            }
+        })
+        .catch((error) => console.log(error));
+    };
 
     return (
         <>
@@ -22,8 +38,12 @@ export default function UserProfile(){
                 <InfoGroup labelValue={"First name"} fieldValue={user.first_name}/>
                 <InfoGroup labelValue={"Last name"} fieldValue={user.last_name}/>
                 <div className="row mt-3 p-3 justify-content-center">
-                    <ProfileButton textValue={"Edit profile"} color={"btn-primary"}/>
-                    <ProfileButton textValue={"Delete account"} color={"btn-danger"}/>
+                    <EditButton/>
+                    <DeleteButton/>
+                    <ModalContainer modalTitle={"Delete profile?"} 
+                                    modalBody={"Are you sure that you want to delete your profile ?"} 
+                                    modalButton={<button className="btn btn-danger" onClick={onDeleteHandler}>Delete</button>}
+                    />
                 </div>
             </div>
         </>
---
+++ frontend/src/components/UserProfileComponents/EditButton.jsx
@@ -0,0 +1,9 @@
+export default function EditButton(){
+    return (
+        <div className="col-4 text-center">
+            <button className="btn btn-primary btn-sm col-7">
+                Edit
+            </button>
+        </div>
+    );
+};

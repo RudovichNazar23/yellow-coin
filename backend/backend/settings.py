6808fc60d14fb91fef9fdbb980c0fcf019b6bf50
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-08-21 18:56:59 +0200

    created a model which implements coins for coin base section

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
-    'user_profile'
+    'user_profile',
+    'coin_base'
 ]
 
 MIDDLEWARE = [
---
+++ backend/coin_base/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Coin(models.Model):
+    title = models.CharField(max_length=100)
+    metal = models.CharField(max_length=100)
+    weight = models.CharField(max_length=100)
+    assay = models.CharField(max_length=100)
+    diameter = models.CharField(max_length=50)
+    country_of_origin = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    circulation = models.CharField(max_length=100)
+    mint = models.CharField(max_length=100)
+    year_of_issue = models.CharField(max_length=100)
+    denomination = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.title}"
+
